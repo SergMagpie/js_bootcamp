7199afc7f8266b888ca9cf625d512227d4886b21
sergmagpie <serhii.sorokin@smartteksas.com>
2022-11-20 12:31:45 +0200

    up

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,36 +15,79 @@ const todos = [{
     completed: true
 }]
 
-const incompleteTodos = todos.filter(function (todo) {
-    return !todo.completed
-})
-
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
-
-todos.forEach(function (todo) {
-    const p = document.createElement('p')
-    if (todo.completed) {
-    p.textContent = todo.text
-} else {
-    const i = document.createElement('i')
-    i.textContent = todo.text
-    p.appendChild(i)
+const sumIncompleteTodos = function () {
+    let complitedTodos = todos.filter(function (todo) {
+        return !todo.completed
+    })
+    return complitedTodos.length
 }
-    document.querySelector('body').appendChild(p)
-})
+const writeSummary = function (){
+const summary = document.createElement('h2')
+summary.textContent = `You have ${sumIncompleteTodos()} todos left`
+document.querySelector('#summary').appendChild(summary)
+}
 
+const writeTable = function () {
+    document.querySelector('#table').replaceChildren()
+    document.querySelector('#summary').replaceChildren()
+    writeSummary()
+    todos.forEach(function (todo) {
+        const p = document.createElement('p')
+        if (!todo.completed) {
+            p.textContent = todo.text
+        } else {
+            const i = document.createElement('s')
+            i.textContent = todo.text
+            p.appendChild(i)
+        }
+        document.querySelector('#table').appendChild(p)
+    })
+    document.querySelectorAll('p').forEach(function (el) {
+        el.addEventListener('click', function (e) {
+            checkTodo(e.target.textContent)
+        })
+    })
+}
+writeTable()
+
+const createNotes = function (e) {
+    const newNote = document.createElement('input')
+
+    document.querySelector('#table').appendChild(newNote).focus()
+    document.querySelector('input').addEventListener('change', function (e) {
+        todos.push({
+            text: e.target.value,
+            completed: false
+        })
+        writeTable()
+    })
+}
 const buttonClick = function (e) {
     console.log('button click')
     console.log(e)
 }
+const checkTodo = function (text) {
+    todos.forEach(function (todo) {
+        if (todo.text === text) {
+            todo.completed = !todo.completed
+        }
+    })
+    writeTable()
+}
 document.querySelector('#create-notes').addEventListener('click', function (e) {
-    buttonClick(e)
+    createNotes(e)
 })
 document.querySelector('#delete-all').addEventListener('click', function (e) {
-    buttonClick(e)
+    todos.length = 0
+    writeTable()
 })
+// document.querySelectorAll('p').forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//         buttonClick(e.target.textContent)
+//     })
+// })
+
+
 
 // You have 2 todos left (p element)
 // Add a p for each todo above (use text value)
