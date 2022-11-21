42aab85274d81c7543c2608d790dc69bb45a0824
sergmagpie <serhii.sorokin@smartteksas.com>
2022-11-21 15:04:10 +0200

    up

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,2 +1,28 @@
-let p = document.querySelector('p')
-p.remove()
+const notes = getSavedNotes()
+
+const filters = {
+    searchText: ''
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    const id = getId()
+
+    notes.push({
+        id: id,
+        title: '',
+        body: ''
+    })
+    saveNotes(notes)
+    location.assign(`/edit.html#${id}`)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
